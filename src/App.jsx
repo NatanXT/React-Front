ceac3e65f2f15207b756cccdf44cd0ce0f70f8b5
Natan Derick <114193508+NatanXT@users.noreply.github.com>
2025-03-19 16:56:55 -0300

    Animation
    Criada uma animação entre duas telas, a home e a login, mas ainda não é bem o que a gente queria, falta melhorias.

--- src/App.jsx
+++ src/App.jsx
@@ -15,20 +15,27 @@ export default App */
 
 
 import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link, useLocation } from 'react-router-dom';
 import Home from './Components/Home/Home';
-import Login from './components/Login/Login';
+import Login from './Components/Login/Login';
+import { AnimatePresence } from "framer-motion"; // Importe o AnimatePresence
 
 const App = () => {
+  const location = useLocation();
+
   return (
     <>
-      <nav>
-        <Link to="/home">Home</Link> | <Link to="/login">Login</Link>
+      <nav style={{ display: "flex", gap: "10px", padding: "10px" }}>
+        | <Link to="/home">Home</Link> |
+        <Link to="/login">Login</Link> |
       </nav>
+
+      <AnimatePresence mode="wait"> {/* AnimatePresence precisa envolver as rotas */}
       <Routes>
         <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
       </Routes>
+      </AnimatePresence>
     </>
   );
 };
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 import './Home.css'; // Arquivo CSS específico para a Home
 
 const Home = () => {
   return (
-    <div className="Home">
-      <h1>Página Principal</h1>
-    </div>
+    <motion.div 
+    className="Home" 
+    initial={{ x: -300, opacity: 0 }}
+    animate={{ x: 0, opacity: 1 }}
+    exit={{ x: 300, opacity: 0 }}
+    transition={{ duration: 0.5 }}
+>
+  <h1>Página Principal</h1>
+</motion.div>
   );
 };
 
